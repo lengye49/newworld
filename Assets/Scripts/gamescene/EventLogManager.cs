646a9f1b9130ba1a98ecfe9d61e9091b7c744277
snake <lib0707@gmail.com>
2018-10-31 08:38:03 +0800

    #Add eventlog manager scripts.

--- Assets/Scripts/gamescene/EventLogManager.cs
+++ Assets/Scripts/gamescene/EventLogManager.cs
@@ -1,16 +1,65 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EventLogManager : MonoBehaviour {
+    private Text[] logs;
+    private int maxCount = 9;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start()
+    {
+        logs = this.gameObject.GetComponentsInChildren<Text>();
+        ClearLogs();
+        AddLog("我真的真的不是你的二大爷。");
+    }
+
+    void ClearLogs()
+    {
+        for (int i = 0; i < logs.Length; i++)
+        {
+            logs[i].text = string.Empty;
+        }
+    }
+
+    public void AddLog(string s, int colorType = 0)
+    {
+        int logCount = (int)s.Length / maxCount + 1;
+        Color c;
+        if (colorType == 0)
+            c = Color.white;
+        else
+            c = colorType == 1 ? Color.green : Color.red;
+
+        string[] ss = new string[logCount];
+        for (int i = 0; i < logCount; i++)
+        {
+            if (i == logCount - 1)
+                ss[i] = s.Substring(i * maxCount, s.Length - i * maxCount);
+            else
+                ss[i] = s.Substring(i * maxCount, maxCount);
+        }
+
+        for (int i = logCount - 1; i >= 0; i--)
+        {
+            AddNewLog(ss[i], c);
+        }
+    }
+
+    void AddNewLog(string s, Color c)
+    {
+        for (int i = logs.Length - 1; i > 0; i--)
+        {
+            logs[i].text = logs[i - 1].text;
+            logs[i].color = logs[i - 1].color;
+            logs[i].color = new Color(logs[i - 1].color.r, logs[i - 1].color.g, logs[i - 1].color.b, GetAlpha(i) / 255f);
+        }
+        logs[0].text = ">" + s;
+        logs[0].color = c;
+    }
+
+    float GetAlpha(int index)
+    {
+        return 255f - index * (255f - 55f) / logs.Length;
+    }
+
 }
