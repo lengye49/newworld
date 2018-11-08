59a728a50bcb3a5d15a75f9c8a7e205b8884cbb3
snake <lib0707@gmail.com>
2018-11-08 15:23:27 +0800

    #add battle ui #add some battle scripts

---
+++ Assets/Scripts/DataManager/BattleUnit.cs
@@ -0,0 +1,25 @@
+﻿
+public class BattleUnit
+{
+    public string name;
+    public int monsterId;
+    public int level;
+    public float hp;
+    public int spirit;
+    public float atk;
+    public float def;
+    public float hit;
+    public float dodge;
+    public float speed;
+    public float range;
+    public Skill[] skillList;
+    public string drop;
+    public int vitalSensibility;
+    public string hit_Body;
+    public string hit_Move;
+    public string hit_Vital;
+    public int canCapture;
+    public float castSpeedBonus;
+    public int mapOpen;
+    public int renown;
+}
---
+++ Assets/Scripts/DataManager/Unit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+
+public class Unit
+{
+    public int id;
+    public string name;
+    public string knickName;
+    public int level;
+    public int model;
+}
+
+public class UnitModel{
+    public int movdelType;
+    public int hp;
+    public int hp_inc;
+    public int atk;
+    public int atk_inc;
+    public int def;
+    public int def_inc;
+    public int hit;
+    public int hit_inc;
+    public int dodge;
+    public int dodge_inc;
+    public int speed;
+    public int speed_inc;
+}
