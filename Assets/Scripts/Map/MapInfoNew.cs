9bd0f50f02d46616d171faa331688ef236dd50fb
snake <lib0707@gmail.com>
2018-11-27 11:41:04 +0800

    #Design Update

---
+++ Assets/Scripts/Map/MapInfoNew.cs
@@ -0,0 +1,21 @@
+﻿public class MapInfoNew
+{
+
+    public int id;
+    public TerrainType type;
+    public bool isDesigned;
+
+    public int xRange;
+    public int yRange;
+    public int[] cellList;
+}
+
+public enum TerrainType{
+    Desert,//沙漠
+    Gobi,//戈壁
+    Land,//土地
+    Grassland,//绿草地
+    Snowfield,//雪地
+    Underwater,//水下
+    Cave,//山洞
+}
